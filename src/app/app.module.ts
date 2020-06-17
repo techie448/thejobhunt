dfd427c20916a5fe9e218b37a95c8f4f14efd556
techie448 <techie448@gmail.com>
2020-06-16 22:25:50 -0400

    add firebase setup to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ import {AngularFireModule} from '@angular/fire';
+ import {environment} from '../environments/environment';
+ import {AngularFirestoreModule} from '@angular/fire/firestore';
+ import {AngularFireAnalyticsModule} from '@angular/fire/analytics';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmZc6M1hf0nT_A6klfwFEB_IrhmQnyfoo",
+    authDomain: "thejobhuntinc-70120.firebaseapp.com",
+    databaseURL: "https://thejobhuntinc-70120.firebaseio.com",
+    projectId: "thejobhuntinc-70120",
+    storageBucket: "thejobhuntinc-70120.appspot.com",
+    messagingSenderId: "691705562088",
+    appId: "1:691705562088:web:0e4ec3ae27ccefe525da5d",
+    measurementId: "G-55YMNHDEBP"
+  }
 };
 
 /*
