251c0bed3dd8aade8a4fda192ed1f2975426eefc
techie448 <techie448@gmail.com>
2020-06-17 00:27:57 -0400

    add jobs module and define job model.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
